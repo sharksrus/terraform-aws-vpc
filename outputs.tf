daca508ba8ebc1f19ce688fe4983f973f1399a61
Glen Stummer <glen@glen.dev>
2020-10-25 13:51:11 -0500

    using single output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,28 @@
-### VPC ###
+output "eip" {
+  value = aws_eip.nat
+}
+
+output "net" {
+  value = aws_nat_gateway.nat
+}
+
+output "subnets_private" {
+  value = aws_subnet.private
+}
+
+output "subnets_public" {
+  value = aws_subnet.public
+}
+
+output "subnets_rds" {
+  count = var.rds_subnet ? 1 : 0
+  value = aws_subnet.rds
+}
+
+output "subnets_storage" {
+  count = var.storage_subnet ? 1 : 0
+  value = aws_subnet.storage
+}
 
 output "vpc" {
   value = aws_vpc.vpc
