5ac46bde104cc9404662ef901e04420b17cc0424
Glen Stummer <glen@glen.dev>
2020-10-25 13:57:23 -0500

    subnet cleanup for tf 13

--- subnets.tf
+++ subnets.tf
@@ -4,7 +4,7 @@ resource "aws_subnet" "public" {
   count             = length(var.availability_zones)
   vpc_id            = aws_vpc.vpc.id
   availability_zone = element(var.availability_zones, count.index)
-  cidr_block        = cidrsubnet("${var.vpc_cidr}", 8, count.index)
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index)
   tags              = merge(local.defaultTags, var.additionalTags, var.public_subnet_tags, map("Name", "public-${element(var.availability_zones, count.index)}-${var.vpc_name}-${var.env}"))
 }
 
@@ -12,7 +12,7 @@ resource "aws_subnet" "storage" {
   count             = var.storage_subnet ? length(var.availability_zones) : 0
   vpc_id            = aws_vpc.vpc.id
   availability_zone = element(var.availability_zones, count.index)
-  cidr_block        = cidrsubnet("${var.vpc_cidr}", 8, 4 + count.index)
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, 4 + count.index)
   tags              = merge(local.defaultTags, var.additionalTags, var.storage_subnet_tags, map("Name", "storage-${element(var.availability_zones, count.index)}-${var.vpc_name}-${var.env}"))
 }
 
@@ -20,7 +20,7 @@ resource "aws_subnet" "rds" {
   count             = var.rds_subnet ? length(var.availability_zones) : 0
   vpc_id            = aws_vpc.vpc.id
   availability_zone = element(var.availability_zones, count.index)
-  cidr_block        = cidrsubnet("${var.vpc_cidr}", 8, 8 + count.index)
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, 8 + count.index)
   tags              = merge(local.defaultTags, var.additionalTags, var.rds_subnet_tags, map("Name", "rds-${element(var.availability_zones, count.index)}-${var.vpc_name}-${var.env}"))
 }
 
@@ -28,6 +28,6 @@ resource "aws_subnet" "private" {
   count             = length(var.availability_zones)
   vpc_id            = aws_vpc.vpc.id
   availability_zone = element(var.availability_zones, count.index)
-  cidr_block        = cidrsubnet("${var.vpc_cidr}", 4, 2 + count.index)
+  cidr_block        = cidrsubnet(var.vpc_cidr, 4, 2 + count.index)
   tags              = merge(local.defaultTags, var.additionalTags, var.private_subnet_tags, map("Name", "private-${element(var.availability_zones, count.index)}-${var.vpc_name}-${var.env}"))
 }
