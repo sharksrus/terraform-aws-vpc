e2d6fe44de422159bd191ed49f544a17ce7e1caa
Glen Stummer <glen@glen.dev>
2020-10-28 20:34:08 -0500

    single output testing

--- outputs.tf
+++ outputs.tf
@@ -3,17 +3,3 @@
 output "vpc" {
   value = aws_vpc.vpc
 }
-
-output "subnets_private" {
-  value = [
-    aws_subnet.private
-  ]
-}
-
-output "nat_gateways" {
-  value = aws_eip.nat
-}
-
-output "nat_gateway_eips" {
-  value = aws_eip.nat
-}
